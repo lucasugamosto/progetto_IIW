c92a81d2402bc5f3a2da5cd06b1b1e789f4b77a6
Luca Sugamosto <63077116+lucasugamosto@users.noreply.github.com>
2021-07-08 01:11:53 +0200

    Add files via upload

--- client_UDP.c
+++ client_UDP.c
@@ -17,8 +17,8 @@ int sd;				        		//socket descriptor
 struct sockaddr_in servaddr;		//struttura per l'indirizzo IP locale e numero di porta locale
 
 int main(int argc, char *argv []) {
-	int result, len;
-	char buffer[maxline];
+	int result, len, c;
+	char buffer[maxline];			//buffer contenente i messaggi di richiesta e di risposta
 	
 	//controllo presenza dell'indirizzo IP passato come argomento della funzione
 	
@@ -26,45 +26,41 @@ int main(int argc, char *argv []) {
 		printf("nessun indirizzo IP inserito\n");
 		exit(-1);
 	}
-	printf("indirizzo IP inserito\n");
 
 	//creazione della socket per il processo client
 
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sd == -1) {
-		perror("errore creazione socket client");
+		perror("errore, creazione socket client fallita\n");
 		exit(-1);
 	}
-	printf("descrittore della socket creata: %d\n", sd);
 
 	//inizializzazione dell'indirizzo IP e del numero di porta
 
 	memset((void *)&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;						//tipo di indirizzo (IPv4)
-	servaddr.sin_port = htons(portnumber);				//assegnazione numero di porta del server
+	servaddr.sin_family = AF_INET;											//tipo di indirizzo (IPv4)
+	servaddr.sin_port = htons(portnumber);									//assegnazione numero di porta del server
 
-	result = inet_aton(argv[1], &(servaddr.sin_addr));	//assegnazione dell'indirizzo del server presente in argv[1]
+	result = inet_aton(argv[1], &(servaddr.sin_addr));						//assegnazione dell'indirizzo del server presente in argv[1]
 	if(result == 0) {
-		perror("errore conversione dell'indirizzo IP\n");
+		perror("errore, conversione dell'indirizzo IP fallito\n");
 		exit(-1);
 	}
-	printf("conversione dell'indirizzo IP riuscita\n");
 
 	while(1) {
 		printf("inserisci comando:  ");
 
-		result = scanf("%s", buffer);
+		result = scanf("%s", buffer);										//inserimento del comando tramite scanf
 		if(result == EOF) {
-			perror("errore lettura comando\n");
+			perror("errore, lettura comando fallita\n");
 			exit(-1);
 		}
 		if(result == 0) {
-			perror("errore compilazione nulla\n");
+			perror("errore, nessun comando inserito\n");
 			exit(-1);
 		}
-		printf("lettura comando convalidata\n");
 
-		//comando LIST
+		//comando LIST (richiesta della lista dei file presenti nel server)
 
 		if(strcmp("list", buffer) == 0) {
 			printf("comando 'list' chiamato\n");
@@ -73,38 +69,111 @@ int main(int argc, char *argv []) {
 
 			result = sendto(sd, buffer, maxline, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
 			if(result == -1) {
-			perror("errore invio richiesta\n");
+			perror("errore, invio richiesta fallito\n");
 			exit(-1);
 			}
 
 			//ricezione risposta corrispondente al comando LIST
 
-			result = recvfrom(sd, buffer, maxline, 0, NULL, NULL);		//con NULL si intende che non si sa chi invia la risposta
+			result = recvfrom(sd, buffer, maxline, 0, NULL, NULL);			//con NULL si intende che non si sa chi invia la risposta
 			if(result == -1) {
-				perror("errore ricezione risposta\n");
+				perror("errore, ricezione risposta fallita\n");
 				exit(-1);
 			}
 
-			printf("%s\n", buffer);										//stampa a schermo della lista dei file nel server
+			printf("\nfile contenuti nel server:\n");
+			printf("%s\n", buffer);											//stampa a schermo la lista dei file nel server
 
-			printf("fine comando LIST nel processo client\n");
+			printf("\n");
 		}
 
-		//comando GET
+		//comando GET (richiesta del contenuto dei file presenti nel server)
 
 		else if(strcmp("get", buffer) == 0) {
 			printf("comando 'get' chiamato\n");
+
+			memset(buffer, 0, sizeof(buffer));								//svuotamento del buffer
+
+			printf("nome del file da richiedere al server: ");
+			scanf(" %[^\n]", buffer);										//permette di inserire stringhe contenente spazi
+
+			c = getchar();
+
+			//invio richiesta corrispondente al comando GET
+
+			result = sendto(sd, buffer, maxline, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+			if(result == -1) {
+				perror("errore, invio richiesta fallito\n");
+				exit(-1);
+			}
+
+			//ricezione risposta corrispondente al comando GET
+
+			result = recvfrom(sd, buffer, maxline, 0, NULL, NULL);			//con NULL si intende che non si sa chi invia la risposta
+			if(result == -1) {
+				perror("errore, ricezione risposta fallita\n");
+				exit(-1);
+			}
+
+			else if(result == 0) {
+				perror("\nfile non presente nel server\n");
+			}
+			else {
+				printf("\ndati contenuti nel file richiesto:\n");
+				printf("%s", buffer);										//stampa a schermo del contenuto del file richiesto
+
+				if(strlen(buffer) >= (maxline-1)) {								//controllo se il messaggio arrivato è completo
+
+					//devono arrivare altre parti del file richiesto
+					
+					while(strlen(buffer) == (maxline-1)) {
+						result = recvfrom(sd, buffer, maxline, 0, NULL, NULL);	//con NULL si intende che non si sa chi invia la risposta
+						if(result == -1) {
+							perror("errore, ricezione risposta fallita\n");
+							exit(-1);
+						}
+						printf("%s", buffer);
+					}
+				}
+			}
+			printf("\n");
 		}
 
-		//comando PUT
+		//comando PUT (upload di un file sul server)
 
 		else if(strcmp("put", buffer) == 0) {
 			printf("comando 'put' chiamato\n");
+
+			memset(buffer, 0, sizeof(buffer));								//svuotamento del buffer
+
+			printf("dati da inserire nel file:\n");
+			scanf(" %[^\n]", buffer);										//permette di inserire stringhe contenente spazi
+
+			c = getchar();
+
+			//invio richiesta corrispondente al comando PUT
+
+			result = sendto(sd, buffer, maxline, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+			if(result == -1) {
+				perror("errore, invio richiesta fallito\n");
+				exit(-1);
+			}
+
+			//ricezione risposta corrispondente al comando PUT
+
+			result = recvfrom(sd, buffer, maxline, 0, NULL, NULL);			//con NULL si intende che non si sa chi invia la risposta
+			if(result == -1) {
+				perror("errore, ricezione risposta fallita\n");
+				exit(-1);
+			}
+
+			printf("%s\n", buffer);
 		}
 
+		//NESSUN COMANDO PRECEDENTE RICHIESTO
+
 		else {
-			perror("errore comando inserito non esistente\n");
-			exit(-1);
+			perror("errore, comando inserito non esistente\n");
 		}
 	}
 
--- server_UDP.c
+++ server_UDP.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/errno.h>
+#include <fcntl.h>			//libreria per le funzioni riguardanti i file
+#include <sys/stat.h>
 
 #include <sys/socket.h>		//definisce i parametri che iniziano per PF e AF
 #include <netinet/in.h>		//definisce i tipi di dato per rappresentare gli indirizzi IP in internet (es. struct sockaddr_in)
@@ -23,16 +25,26 @@ struct dirent *dp;			//struttura per la gestione dei file del server
 
 int main(int argc, char *argv[1]) {
 	int result, len;
-	char buffer[maxline];
+	int fd;					//file descriptor
+	char buffer[maxline];	//buffer contenente i messaggi di richiesta e di risposta
+	char *pathname;			//nome del percorso per il file
+	size_t val;
+
+	//allocazione di memoria oer la variabile richiesta nel comando GET
+
+	pathname = (char *)malloc(256);
+	if(pathname == NULL) {
+		perror("errore, allocazione di memoria fallita\n");
+		exit(-1);
+	}
 
 	//creazione della socket per il processo server (usata solo per l'ascolto di richieste)
 
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sd == -1) {
-		perror("errore creazione socket client\n");
+		perror("errore, creazione socket fallita\n");
 		exit(-1);
 	}
-	printf("descrittore della socket creata: %d\n", sd);
 
 	//inizializzazione dell'indirizzo IP e del numero di porta
 
@@ -45,7 +57,7 @@ int main(int argc, char *argv[1]) {
 
 	result = bind(sd, (struct sockaddr *)&sad, sizeof(sad));
 	if(result == -1) {
-		perror("errore inizializzazione processo\n");
+		perror("errore, inizializzazione processo locale fallita\n");
 		exit(-1);
 	}
 
@@ -58,11 +70,9 @@ int main(int argc, char *argv[1]) {
 
 		result = recvfrom(sd, buffer, maxline, 0, (struct sockaddr *)&sad, &len);
 		if(result == -1) {
-			perror("errore ricezione richiesta\n");
+			perror("errore, ricezione richiesta fallita\n");
 			exit(-1);
 		}
-
-		printf("%s\n", buffer);
 
 		//comando LIST
 
@@ -72,15 +82,14 @@ int main(int argc, char *argv[1]) {
 			memset(buffer, 0, sizeof(buffer));					//svuotamento del buffer
 
 			if ((dir = opendir ("server UDP")) == NULL) {		//controllo dei file presenti all'interno del server 
-        		perror ("errore lettura file nel server");
+        		perror ("errore, lettura file nel server fallita\n");
         		exit (-1);
     		}
 			
 			while ((dp = readdir (dir)) != NULL) {				//lettura della directory appena creata contenente i nomi dei file
         		if(dp->d_type == DT_REG) {						//controllo del tipo di elemento da inserire nel buffer
-            		//printf("%s\n", dp->d_name);
             		strcat(buffer, dp->d_name);					//inserimento del file i-esimo all'interno del buffer
-            		strcat(buffer, "\n");					
+            		strcat(buffer, ", ");				
         		}
         	}
 
@@ -90,13 +99,95 @@ int main(int argc, char *argv[1]) {
 
 			result = sendto(sd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&sad, sizeof(sad));
 			if(result == -1) {
-				perror("errore invio risposta\n");
+				perror("errore, invio risposta fallito\n");
 				exit(-1);
 			}
 		}
 
-		printf("nessun comando inserito\n");
-	}
+		//comando PUT
 
+		else if(strcmp("put", buffer) == 0) {
+			printf("creazione del file\n");
+
+		}
+
+		//comando GET
+
+		else {
+			//controllo la presenza del file richiesto dal client scorrendo la lista dei file del server
+			printf("richiesta lettura del contenuto di '%s'\n", buffer);
+
+			if ((dir = opendir ("server UDP")) == NULL) {				//controllo dei file presenti all'interno del server 
+        		perror ("errore, lettura file nel server fallita\n");
+        		exit (-1);
+    		}
+
+			result = 0;													 //impongo variabile per definire la presenza o meno del file
+
+			while ((dp = readdir (dir)) != NULL) {						 //lettura della directory appena creata contenente i nomi dei file
+        		if(dp->d_type == DT_REG) {								 //controllo del tipo di elemento da inserire nel buffer
+            		if(strcmp(dp->d_name, buffer) == 0) {				 //controllo se file richiesto è presente nel server
+
+            			result = 1;
+
+            			strcpy(pathname, "server UDP/");
+            			strcat(pathname, dp->d_name);
+
+            			fd = open(pathname, O_RDONLY, 0660);	 		 //apertura del file richiesto dal client
+            			if(fd == -1) {
+            				perror("errore, apertura del file fallita\n");
+            				exit(-1);
+            			}
+            			else {
+            				memset(buffer, 0, sizeof(buffer));			 //svuotamento del buffer
+
+            				val = read(fd, buffer, (maxline-1));		 //lettura dei dati dal file ed inserimento del buffer
+
+            				if(val == -1) {
+            					perror("errore, lettura fallita\n");
+            					exit(-1);
+            				}
+        					else {
+        						result = sendto(sd, buffer, maxline, 0, (struct sockaddr *)&sad, sizeof(sad));
+        						if(result == -1) {
+									perror("errore, invio risposta fallito\n");
+									exit(-1);
+								}
+   							}
+
+   							if(strlen(buffer) >= (maxline-1)){
+
+   								//il file non è stato completamente inviato con un solo messaggio
+
+   								while(strlen(buffer) == (maxline-1)) {			 //finchè il buffer si riempie
+   									memset(buffer, 0, sizeof(buffer));			 //svuotamento del buffer
+
+   									if(read(fd, buffer, (maxline-1)) == -1) {    //lettura dei dati dal file ed inserimento del buffer
+            							perror("errore, lettura fallita\n");
+            							exit(-1);
+            						}
+									result = sendto(sd, buffer, maxline, 0, (struct sockaddr *)&sad, sizeof(sad));
+        							if(result == -1) {
+										perror("errore, invio risposta fallito\n");
+										exit(-1);
+									}
+   								}	
+   							}
+   						}
+            		}
+        		}
+        	}
+        	if(result == 0) {
+        		//caso in cui il file non è presente nel server e viene inviato un buffer vuoto
+
+        		result = sendto(sd, NULL, 0, 0, (struct sockaddr *)&sad, sizeof(sad));
+        		if(result == -1) {
+        			perror("errore, invio risposta fallito\n");
+        			exit(-1);
+        		}
+			}
+        	closedir(dir);												 //chiusura della directory dir
+		}
+	}
 	return(0);
 }
